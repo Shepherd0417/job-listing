b04ebb0ac72e76450c7ccf5fa6afb4338aa3d52f
Shepherd <sunnan0417@gmail.com>
2017-05-01 01:42:52 +0800

    step 7 upload resume

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  resources :jobs
   root 'jobs#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :admin do
@@ -11,5 +10,9 @@ Rails.application.routes.draw do
         post :hide
       end
     end
+  end
+
+  resources :jobs do
+    resources :resumes
   end
 end
