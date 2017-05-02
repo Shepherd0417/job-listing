16ff43026fc762fcdfbedc91e43886dc90f9c8a2
Shepherd <sunnan0417@gmail.com>
2017-05-02 12:50:09 +0800

    revised the flash info

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,5 @@
 class WelcomeController < ApplicationController
   def index
-    flash[:warning] = "5月份之前咱要把网站做出来啊"
+    flash[:notice] = "Hello!"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  root 'jobs#index'
+  root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :admin do
     resources :jobs do
